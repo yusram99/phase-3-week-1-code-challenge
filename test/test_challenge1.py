d1eeadd0e6e9d08378217a4545f91340a2554456
Your Name <youremail@example.com>
2023-08-21 15:05:13 +0300

    completed assignment

--- test/test_challenge1.py
+++ test/test_challenge1.py
@@ -1,5 +1,5 @@
 import pytest
-from test.challenge1.challenge1 import convert_to_24_hour_format
+from challenge1 import convert_to_24_hour_format
 
 def test_convert_to_24_hour_format():
     assert convert_to_24_hour_format(8, 30, "am") == "0830"
